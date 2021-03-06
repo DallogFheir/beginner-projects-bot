7eba99eb85ce8075364a1f9e611ddbd2bd94b11c
DallogFheir <69858208+DallogFheir@users.noreply.github.com>
2021-03-06 22:35:22 +0100

    updated regex

--- comment_parser/texts.py
+++ comment_parser/texts.py
@@ -1,23 +1,23 @@
 import re
 
-#region REPLY TEXT
+# region REPLY TEXT
 MAIN_TEXT = """1. Create a bot to reply to "what are some beginner projects" questions on r/learnpython, using [PRAW](https://praw.readthedocs.io/en/latest/index.html).
 
 Other than that, [here's](https://github.com/karan/Projects) a list of programming projects on Github. [Here's](https://web.archive.org/web/20180612183650if_/https://github.com/jorgegonzalez/beginner-projects) another archived list. Also check out [this resource](https://www.reddit.com/r/learnpython/wiki/index#wiki_tools_for_learning_python) in the subreddit wiki. Good luck!"""
 
 SMALL_TEXT = """^(Downvote me if the post wasn't a question about examples of beginner projects. Thank you.)"""
-#endregion
+# endregion
 
-#region UPVOTES TEXTS
+# region UPVOTES TEXTS
 UPVOTES_TEXTS = {
-  "5": "thanks for 5 upvotes!",
-  "10": "omg 10 upvotes!!!! Thank you!!",
-  "50": "50 upvotes??? 😲😲😲 Can we make it to 100?",
-  "100": "100 UPVOTES?????? I CAN DIE NOW"
+    "5": "thanks for 5 upvotes!",
+    "10": "omg 10 upvotes!!!! Thank you!!",
+    "50": "50 upvotes??? 😲😲😲 Can we make it to 100?",
+    "100": "100 UPVOTES?????? I CAN DIE NOW",
 }
-#endregion
+# endregion
 
-#region REPLY TO JUDGMENT TEXTS
+# region REPLY TO JUDGMENT TEXTS
 REPLY_TO_PRAISE_TEXT = """Praise for the food is praise for the cook.
 
 Thanks from the programmer."""
@@ -26,17 +26,17 @@ REPLY_TO_CRITICISM_TEXT = """:(
 
 I'm open to criticism, please message me and tell me what you don't like about me.
 """
-#endregion
+# endregion
 
-#region REPLY TO COMPETITION TEXT
+# region REPLY TO COMPETITION TEXT
 REPLY_TO_COMPETITION_TEXT = """Great minds think alike!"""
-#endregion
+# endregion
 
-#region HUMAN COMMENT TEXT
+# region HUMAN COMMENT TEXT
 HUMAN_COMMENT_TEXT = "beep boop I'm a human"
-#endregion
+# endregion
 
-#region REGEX PATTERNS
+# region REGEX PATTERNS
 AWARD_TEXT = "Thank you for the (.*), kind stranger!"
 AWARD_PATTERN = re.compile(AWARD_TEXT)
 
@@ -45,6 +45,8 @@ _title_text = r"""
     # ignore words
     (?!
     .*troubleshooting
+    |
+    .*for\ my
     |
     \d # listicles like 10 beginner projects
     |
@@ -86,11 +88,11 @@ _title_text = r"""
     )
     )
 """
-TITLE_PATTERN = re.compile(_title_text,re.I|re.X)
+TITLE_PATTERN = re.compile(_title_text, re.I | re.X)
 
 _praise_text = "good bot[.!]?"
-PRAISE_PATTERN = re.compile(_praise_text,re.I)
+PRAISE_PATTERN = re.compile(_praise_text, re.I)
 
 _criticism_text = "bad bot[.!]?"
-CRITICISM_PATTERN = re.compile(_criticism_text,re.I)
-#endregion
+CRITICISM_PATTERN = re.compile(_criticism_text, re.I)
+# endregion
