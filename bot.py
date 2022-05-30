1102ca082a09c8f27dc8c11edbd38b78ed267e09
DallogFheir <dallog.fheir@gmail.com>
2022-05-30 04:18:46 +0200

    fixed path

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ import re
 
 # region CONFIG
 version_pattern = re.compile(r"## Changelog\n\n- (\d+\.\d+\.\d+)")
-with open(Path(__file__) / "README.md") as f:
+with open(Path(__file__).parent / "README.md") as f:
     version = re.search(version_pattern, f.read())[1]
 USER_AGENT = f"script:beginner-projects-bot:v{version} (by /u/DallogFheir)"
 
